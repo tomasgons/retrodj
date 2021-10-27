70c29c787817e00d424d3eb39d407dde71215a9f
gonzocode <tomasgons@gmail.com>
2021-10-27 12:35:18 +0200

    first

---
+++ script.js
@@ -0,0 +1,33 @@
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {
+    slideIndex = 1;
+  }
+  x[slideIndex - 1].style.display = "inline";
+  setTimeout(carousel, 3000); // Change image every 3 seconds
+}
+
+var slideIndex2 = 0;
+carousel2();
+
+function carousel2() {
+  var i;
+  var x = document.getElementsByClassName("mySlides2");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex2++;
+  if (slideIndex2 > x.length) {
+    slideIndex2 = 1;
+  }
+  x[slideIndex2 - 1].style.display = "inline";
+  setTimeout(carousel2, 3000); // Change image every 3 seconds
+}
